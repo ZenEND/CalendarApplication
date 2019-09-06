7810d69af81a97f800f5a8edd3990bdce38db664
ZenEnd <o.s.maksimenko@gmail.com>
2019-09-06 12:47:48 +0300

    feat: configs to build

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({})
+const debug = process.env.NODE_ENV !== "production";
+module.exports = withSass({
+    exportPathMap: function () { // /Next-React-Components
+        return {
+        "/": { page: "/" },
+        "/ap-grid-layout": { page: "/ap-grid-layout" },
+        "/ap-highlight": { page: "/ap-highlight" },
+        }
+    },
+    assetPrefix: !debug ? 'https://ZenEnd.github.io/CalendarApplication/' : '',})
