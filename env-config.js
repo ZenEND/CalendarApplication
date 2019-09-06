a922a0aa02692a7705a455f7535b26a29c0cc86d
ZenEnd <o.s.maksimenko@gmail.com>
2019-09-06 14:58:49 +0300

    heroku build

--- env-config.js
+++ env-config.js
@@ -8,16 +8,5 @@ const assetPrefix = isProd ? '/your-repository-name' : ''
 module.exports = withSass({
   exportPathMap: () => ({
     '/': { page: '/' },
-    '/page1': { page: '/page1' },
-  }),
-  assetPrefix: assetPrefix,
-  webpack: config => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-      }),
-    )
-
-    return config
-  },
+  })
 })
--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 const withSass = require('@zeit/next-sass')
 const isProd = (process.env.NODE_ENV || 'production') === 'production'
-module.exports = withSass({
-    exportPathMap: function () { // /Next-React-Components
-        return {
-        "/": { page: "/" }
-        }
-    },
-    assetPrefix: isProd ? 'https://ZenEnd.github.io/CalendarApplication/' : '',})
+module.exports = withSass({})
